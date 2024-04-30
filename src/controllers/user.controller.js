0b73a2ffc1b206c9a80b1d5e8d5b4d84a621ea3e
Melly Lucas <melly.lucas32@yahoo.fr>
2024-04-30 15:10:17 +0200

    user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+var secret = process.env.JWT_SECRET;
+
 
 
 exports.register = async(req, res) => {
@@ -16,26 +18,38 @@ exports.register = async(req, res) => {
     //si l'adresse existe déjà c'est qu'il y a déjà un compte donc renvoyer déjà un compte car la fonction se crache mais faut préciser pourquoi
     else{
       try{
-
         req.body.Mdp = await bcrypt.hash(req.body.Mdp, 10);
-        console.log(req.body)
-        const user = await prisma.user.create({
-          data: req.body,
-        });
+     
+       
+        const users = await prisma.user
+        .findUnique({
+           where: {
+             Email: req.body.Email,
+           },
+         })
+         if (users){
+          res.status(200).send({register:false, message:"Un compte est déjà rattaché à cette adresse mail"})
+         }
+         else{
+            const user = await prisma.user.create({
+              data: req.body,
+            });
+            
+            
+            let userToken = jwt.sign(
+            {
+              id: user.Id_Utilisateur,
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: 86400,
+            },
+            )
+            res.status(200).send({ register: true, data: user, token: userToken });
+         }
+        // console.log(userToken, "usertoken")
+        // console.log(process.env.JWT_SECRET)
         
-        let userToken = jwt.sign(
-        {
-          id: user.Id_Utilisateur,
-        },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: 86400,
-        },
-        )
-        
-        console.log(userToken, "usertoken")
-        console.log(process.env.JWT_SECRET)
-        res.status(200).send({ register: true, data: user, token: userToken });
       }
       catch(err){
         res.status(500).send({ register: false, message: err });
@@ -48,6 +62,44 @@ exports.register = async(req, res) => {
   }
 }
 
+
+exports.login = async(req, res) => {
+  try{
+    const user = await prisma.user
+    .findUnique({
+       where: {
+         Email: req.body.Email,
+       },
+     })
+     let passwordValid = bcrypt.compareSync(req.body.Mdp, user.Mdp)
+     if (!passwordValid) {
+        return res.status(401).send({
+            message: "password not valid",
+            auth: false,
+            token: null
+        })
+      }
+      console.log(user, "USER")
+      let userToken = jwt.sign({
+          id: user.Id_Utilisateur,
+      },
+      secret,
+      {
+          expiresIn: 86400
+      })
+      res.status(200).send({
+          auth: true,
+          token: userToken,
+          // username: user.username,
+          // id: user._id,
+          // image: user.image,
+      })
+  }
+  catch(err){
+    res.status(404).send({ error: `err : ${err}` })
+  }
+  
+}
 
 exports.getAll = async (req, res) => {
   try {
@@ -65,3 +117,35 @@ exports.getAll = async (req, res) => {
   }
 };
 
+
+exports.verifyToken = (req, res, next) => {
+    
+  let token = req.headers.authorization
+
+  if (!token) {
+    
+    return res.status(403).send({
+        auth : false,
+        token : null,
+        message : 'Missing token'
+    })
+  }
+  jwt.verify(token, process.env.JWT_SECRET,
+  function(error, jwtdecoded) { 
+
+    if (error) {
+        next();
+        return res.status(401).send({
+            auth : false,
+            token : null,
+            message : "not authorized"
+        })
+    }
+    else{
+        return res.status(200).send({
+            auth : true,
+        })
+    }
+    
+  })
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/user.controller')
 
 router.get('/', userController.getAll);
-router.post('/', userController.register);
-
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/verifyToken', userController.verifyToken);
 
 module.exports = router;
